046188acd91c38df299ab4775a6407ddd26a3caf
kmax.eth <max.kaneco@gmail.com>
2023-07-04 21:35:08 +0800

    comment

--- car.go
+++ car.go
@@ -10,17 +10,21 @@ import (
 	selectorparse "github.com/ipld/go-ipld-prime/traversal/selector/parse"
 )
 
+// Builder builds a WriterTo from the given input source.
+// The WriterTo can be used to output car format data to a io.Writer.
 type Builder struct {
 	di *DataImporter
 	wt io.WriterTo
 }
 
+// NewBuilder creates a new car builder.
 func NewBuilder() *Builder {
 	return &Builder{
 		di: NewDataImporter(),
 	}
 }
 
+// Build builds a WriterTo for outputing car format data.
 func (b *Builder) Build(
 	ctx context.Context,
 	input any,
